1de7feb817280c2b9d591f17cd838bf50fd6ff67
Smellyslab <76969866+Smellyslab@users.noreply.github.com>
2021-02-11 08:30:25 -0500

    Update print.go

--- print.go
+++ print.go
@@ -2,18 +2,8 @@ package py
 
 import (
   "fmt"
-  "net/http"
 )
 
-
-
-// it is making me use the net/http packge for some reason so just ignore this
-func whywhywhywhywhwywhw() {
-  
-  buh := http.Get("https://buh.com")
-
-
-}
 
 
 func Print(text string) {
